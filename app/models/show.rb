4bd43a3dffad77092009e9715c51d9125f6e7bb9
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-15 15:59:14 -0600

    commit tree 4f4fe7f520cb6b68fdc5839e04da81abfe7292c8

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    characters.map { |char| char.actor.full_name.to_s }
+  end
 end
