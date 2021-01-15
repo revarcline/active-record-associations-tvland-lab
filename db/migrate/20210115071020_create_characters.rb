34ff6aac24661bd7a3d7457212119a5c76d328fe
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-15 15:59:18 -0600

    Done.

---
+++ db/migrate/20210115071020_create_characters.rb
@@ -0,0 +1,10 @@
+# sometimes they play characters
+class CreateCharacters < ActiveRecord::Migration[6.0]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+    end
+  end
+end
